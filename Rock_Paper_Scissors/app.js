961c05a350dfbd0bd38aef8162c86523b07ed078
julifmontoya <36722871+julifmontoya@users.noreply.github.com>
2020-08-10 23:15:01 -0500

    Add files via upload

--- Rock_Paper_Scissors/app.js
+++ Rock_Paper_Scissors/app.js
@@ -10,6 +10,24 @@ const rock = document.getElementById("r");
 const paper = document.getElementById("p");
 const scissors = document.getElementById("s");
 
+document.addEventListener("DOMContentLoaded", function () {
+  const timeLeftDisplay = document.getElementById("time-left");
+  const startBtn = document.getElementById("start-button");
+  let timeLeft = 3;
+
+  function countDown() {
+    setInterval(function () {
+      if (timeLeft <= 0) {
+        clearInterval((timeLeft = 0));
+      }
+      timeLeftDisplay.innerHTML = timeLeft;
+      timeLeft -= 1;
+    }, 1000);
+  }
+
+  startBtn.addEventListener("click", countDown);
+});
+
 function getComputerChoice() {
   const choices = ["r", "p", "s"];
   const randomNumber = Math.floor(Math.random() * 3);
